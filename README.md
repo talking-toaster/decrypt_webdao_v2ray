757a6f06431b73da7fb296a49483c868aef4a427
talkingtoaster <xz.xuezhen@gmail.com>
2021-11-19 23:43:47 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Decrypt webdao v2ray
+
+XOR
+
+`key = yyyymmdd*4`
+
+```
+app-armeabi-v7a-release.apk
+
+smali_classes2\xyz\webdao\v2ray\util\EncryptUtil.smali smali_classes2\xyz\webdao\v2ray\util\HttpUtil.smali
+```
+
+```java
+byte[] decode = Base64.getDecoder().decode(node);
+String node1 = new String(decode);
+char[] charArray = key.toCharArray();
+StringBuilder sb = new StringBuilder();
+int length = node1.length();
+for (int i = 0; i < length; i++) {
+    sb.append((char) (node1.charAt(i) ^ charArray[i % charArray.length]));
+}
+```
---
+++ main.py
@@ -0,0 +1,38 @@
+import datetime
+import base64
+import requests
+
+
+def get_raw_link():
+    url = 'https://raw.githubusercontent.com/webdao/v2ray/master/nodes.txt'
+
+    try:
+        response = requests.get(url)
+        if response.status_code != 200:
+            print(response.text)
+            raise Exception("Network Error")
+    except:
+        print("Cannot connect to raw.githubusercontent.com")
+        exit()
+
+    return response.text
+
+
+def get_key(date: datetime.date = datetime.date.today()):
+    yyyymmdd = str(date.year)+str(date.month)+str(date.day)
+    return yyyymmdd*4
+
+
+def decrypt(key: str, ciphertext: str or bytes):
+    '''
+    XOR
+    '''
+    b64_cipher = base64.b64decode(ciphertext).decode()
+    plaintext = ''
+    for i in range(len(b64_cipher)):
+        plaintext += chr(ord(b64_cipher[i]) ^ ord(key[i % len(key)]))
+    return plaintext
+
+
+link = decrypt(get_key(), get_raw_link())
+print(link)
