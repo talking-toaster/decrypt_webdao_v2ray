5879474599c192da76f0d0505b6bf2160c881e6b
talking-toaster <49156890+talking-toaster@users.noreply.github.com>
2021-11-21 14:55:18 +0800

    update

--- update.py
+++ update.py
@@ -3,4 +3,6 @@ from main import *
 
 node = base64.b64encode(decrypt(get_key(), get_raw_link()).encode()).decode()
 
+with open (os.path.join(os.getcwd(), "node.txt"), 'w') as f:
+    f.write(node)
 print(node)
